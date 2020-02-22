689c03246d2411607e0f85052d2c946e62dd364d
Marcel Swoboda <marcel.swoboda@gmx.de>
2020-02-22 22:23:46 +0100

    security

--- src/test/java/org/kizombadev/spring/demo/controller/DemoControllerTest.java
+++ src/test/java/org/kizombadev/spring/demo/controller/DemoControllerTest.java
@@ -5,6 +5,7 @@ import org.kizombadev.spring.demo.service.DemoRestClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.when;
@@ -21,6 +22,7 @@ public class DemoControllerTest {
     @MockBean
     private DemoRestClientService demoRestClientService;
 
+    @WithMockUser
     @Test
     public void getDemo() throws Exception {
         final String foo = "foo";
--- src/test/java/org/kizombadev/spring/demo/controller/UserControllerTest.java
+++ src/test/java/org/kizombadev/spring/demo/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import org.kizombadev.spring.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.eq;
@@ -23,6 +24,7 @@ public class UserControllerTest {
     @MockBean
     private UserService userService;
 
+    @WithMockUser
     @Test
     public void getUser() throws Exception {
 
